acd4ac1e134420f864ce1ba0f8dac9e412d3925f
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-05-21 13:24:46 +0200

    Remove Webpack

--- src/js/app.js
+++ src/js/app.js
@@ -24,10 +24,10 @@ const prolificID = new URL(document.location.href).searchParams.get(
   "PROLIFIC_PID"
 );
 
-// attach it to the button
+// attach it to the button & add a unix timestamp
 button.setAttribute(
   "onclick",
-  `location.href='https://ccp-odc.eva.mpg.de/gafo-prolific/experiment.html?PROLIFIC_PID=${prolificID}'`
+  `location.href='https://ccp-odc.eva.mpg.de/gafo-prolific/experiment.html?PROLIFIC_PID=${prolificID}&time=${Date.now()}'`
 );
 
 // const url = new URL(document.location.href);
---
+++ src/js/webcam.js
@@ -0,0 +1,1 @@
+import { silentrecorder } from "@ccp-eva/silent-recorder";
