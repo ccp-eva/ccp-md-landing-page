2630950aa3bae3b37e7f7ae938a580a738e07129
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-05-23 21:03:50 +0200

    Add special codde

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("confirm-btn");
+const checkbox = document.getElementById("confirm-checkbox");
+
+const handleChecked = () => {
+  button.disabled = !checkbox.checked;
+};
+
+document
+  .querySelector(".mdc-checkbox")
+  .addEventListener("click", handleChecked);
+
+// get and store prolific id
+const prolificID = new URL(document.location.href).searchParams.get(
+  "PROLIFIC_PID"
+);
+
+// remove ID from URL
+window.history.replaceState({}, document.title, "/src/index.html");
+
+// attach it to the button & add a unix timestamp
+button.setAttribute(
+  "onclick",
+  `location.href='/src/webcam.html?PROLIFIC_PID=${prolificID}&time=${Date.now()}'`
+);
